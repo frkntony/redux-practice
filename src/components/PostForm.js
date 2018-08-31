3902b0b3ef58352f2c6c6a5c288d48600af9f1d6
frkntony <tony.elistratov@gmail.com>
2018-08-31 23:05:53 +0300

    done

---
+++ src/components/PostForm.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createPost } from '../actions/postActions'
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: '',
+      body: ''
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    this.props.createPost(post)
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Title: </label> <br />
+            <input type='text' name='title' onChange={this.onChange} value={this.state.title} />
+          </div>
+          <div>
+            <label>Body: </label> <br />
+            <input type='textarea' name='body' onChange={this.onChange} value={this.state.body} />
+          </div>
+          <br />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect (null, { createPost })(PostForm)
